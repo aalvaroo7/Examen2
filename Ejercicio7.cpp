f9d1eebd2ace8401d5812fdfc349b935f0cd5333
aalvaroo7 <almarrom@myuax.com>
2023-12-05 11:45:09 +0100

    comit

---
+++ Ejercicio7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class Variant {
+public:
+    // Definir un tipo para representar valores flexibles
+    using Value = std::variant<int, double, std::string>;
+
+    // Constructor que acepta un valor inicial
+    Variant(const Value& val) : value(val) {}
+
+    // Obtener el valor almacenado en Variant
+    Value getValue() const {
+        return value;
+    }
+
+    // Imprimir el valor almacenado
+    void printValue() const {
+        std::visit([](const auto& v) { std::cout << v; }, value);
+        std::cout << std::endl;
+    }
