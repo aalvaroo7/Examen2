9479c9acc9c2e8c27cd8cb8d7bb951413585143e
aalvaroo7 <almarrom@myuax.com>
2023-12-05 10:50:39 +0100

    comit

--- EJ1.cpp
+++ EJ1.cpp
@@ -13,3 +13,11 @@ public:
         // Agrega más tipos según sea necesario
         //definicion de la clase variant que puede ser int, double o string
     };
+private:
+    Type valueType;
+    union {
+        int intValue;
+        double doubleValue;
+        std::string stringValue;
+        // Agrega más miembros de la unión según sea necesario
+    };
