b4e7783c895613cb45185b4ef978a42dcb0ff425
aalvaroo7 <almarrom@myuax.com>
2023-12-05 11:30:43 +0100

    comit

--- ejemploexplicacion5.cpp
+++ ejemploexplicacion5.cpp
@@ -10,3 +10,22 @@ public:
         DOUBLE,
         STRING
     };
+private:
+    Type valueType;
+    union {
+        int intValue;
+        double doubleValue;
+        std::string stringValue;
+    };
+public:
+    // Constructores para cada tipo de valor
+    Variant(int value) : valueType(Type::INT), intValue(value) {}
+    Variant(double value) : valueType(Type::DOUBLE), doubleValue(value) {}
+    Variant(const std::string& value) : valueType(Type::STRING), stringValue(value) {}
+
+    // Métodos para obtener el valor y el tipo
+    int getInt() const { return intValue; }
+    double getDouble() const { return doubleValue; }
+    std::string getString() const { return stringValue; }
+    Type getType() const { return valueType; }
+};
