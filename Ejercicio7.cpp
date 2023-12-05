30399dadb624b838cff1f2fcee0578a9e21ccc80
aalvaroo7 <almarrom@myuax.com>
2023-12-05 11:45:23 +0100

    comit

--- Ejercicio7.cpp
+++ Ejercicio7.cpp
@@ -19,3 +19,28 @@ public:
         std::visit([](const auto& v) { std::cout << v; }, value);
         std::cout << std::endl;
     }
+private:
+    Value value;
+};
+
+class Environment {
+public:
+    // Método para agregar un símbolo y su valor a la tabla de símbolos
+    void addSymbol(const std::string& symbol, const Variant::Value& value) {
+        symbolTable[symbol] = value;
+    }
+
+    // Método para obtener el valor asociado a un símbolo en la tabla de símbolos
+    Variant::Value getSymbolValue(const std::string& symbol) const {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            return it->second; // Devuelve el valor asociado al símbolo
+        } else {
+            std::cerr << "Error: El símbolo '" << symbol << "' no está definido." << std::endl;
+            return Variant::Value{}; // Devuelve un valor predeterminado
+        }
+    }
+
+private:
+    std::map<std::string, Variant::Value> symbolTable;
+};
