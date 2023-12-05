5c54cf5013ea35bc6edb87f4d09182c9eb086512
aalvaroo7 <almarrom@myuax.com>
2023-12-05 11:00:34 +0100

    cambie todo el codigo otra vez (soy bobo)

--- EJ1.cpp
+++ EJ1.cpp
@@ -2,34 +2,32 @@
 #include <string>
 #include <iostream>
 
-using namespace std;
-
 class Environment {
 public:
     // Constructor
     Environment() {}
 
-    // Destructor
+    // Destructor (no es necesario en este caso, se proporciona como ejemplo)
     ~Environment() {}
 
     // Función para agregar un símbolo y su valor al entorno
-    void addSymbol(const string& name, int value) {
+    void addSymbol(const std::string& name, int value) {
         symbolTable[name] = value;
     }
 
     // Función para obtener el valor de un símbolo desde el entorno
-    int getSymbolValue(const string& name) {
-        // Si el símbolo no está presente, devuelve un valor predeterminado (puedes ajustarlo según tu necesidad)
-        if (symbolTable.find(name) != symbolTable.end()) {
-            return symbolTable[name];
+    int getSymbolValue(const std::string& name) const {
+        auto it = symbolTable.find(name);
+        if (it != symbolTable.end()) {
+            return it->second;
         } else {
-            cerr << "Error: Symbol '" << name << "' not found in the environment." << endl;
+            std::cerr << "Error: Symbol '" << name << "' not found in the environment." << std::endl;
             return 0; // Puedes cambiar esto según tus necesidades
         }
     }
 
 private:
-    map<string, int> symbolTable;
+    std::map<std::string, int> symbolTable;
 };
 
 int main() {
@@ -44,11 +42,11 @@ int main() {
     int valueX = env.getSymbolValue("x");
     int valueY = env.getSymbolValue("y");
 
-    cout << "Value of x: " << valueX << endl;
-    cout << "Value of y: " << valueY << endl;
+    std::cout << "Value of x: " << valueX << std::endl;
+    std::cout << "Value of y: " << valueY << std::endl;
 
     // Intentar obtener un símbolo que no está en el entorno
     int valueZ = env.getSymbolValue("z");
 
-        return 0;
+    return 0;
 }
